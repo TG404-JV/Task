d7acf0bbab498623f0be18f61e24216eb326820d
TG404-JV <tejaskale06032002@gmail.com>
2025-07-30 18:14:29 +0530

    added new features

--- app/src/main/java/com/tg/societymanagment/fragments/task/AddTask.kt
+++ app/src/main/java/com/tg/societymanagment/fragments/task/AddTask.kt
@@ -1,11 +1,13 @@
 package com.tg.societymanagment.fragments.task
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
 import com.tg.societymanagment.R
@@ -48,24 +50,43 @@ class AddTask : Fragment() {
         }
 
         binding.etStartTime.setOnClickListener {
-            val picker = MaterialTimePicker.Builder()
-                .setTimeFormat(TimeFormat.CLOCK_12H)
-                .setHour(10)
-                .setMinute(10)
-                .setTitleText("Select Time")
-                .build()
+            showTimePicker(binding.etStartTime)
+        }
 
-            picker.show(parentFragmentManager,"timePicker")
+        binding.etEndTime.setOnClickListener {
+            showTimePicker(binding.etEndTime)
+        }
+    }
 
-            picker.addOnPositiveButtonClickListener {
-                val hour = picker.hour
-                val minute = picker.minute
-                val time = "$hour:$minute"
-                binding.etStartTime.setText(time)
+
+    private fun showTimePicker(view : TextInputEditText) {
+        val picker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(10)
+            .setMinute(10)
+            .setTitleText("Select Time")
+            .build()
+
+        picker.show(parentFragmentManager,"timePicker")
+        picker.addOnPositiveButtonClickListener {
+            var ampm: String
+            var hour = picker.hour
+            if (picker.hour>12)
+            {
+                hour -=12
+                ampm = "PM"
+            }
+            else
+            {
+                ampm = "AM"
             }
 
 
+            view.text = Editable.Factory.getInstance().newEditable("${hour}:${picker.minute} $ampm")
         }
+
+
+
     }
 
 }
